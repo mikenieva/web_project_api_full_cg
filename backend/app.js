cd401d8480065f82af3a6aa7dbea488ca0e24f80
Carlos Gomez <cgomezlugo@hotmail.com>
2024-01-28 17:45:48 -0400

    cors

--- backend/app.js
+++ backend/app.js
@@ -38,20 +38,6 @@ app.use(requestLogger);
 app.use(cors());
 app.options('*', cors());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-  );
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
-    return res.status(200).json({});
-  }
-  next();
-  return null;
-});
-
 app.post('/signup', createUser);
 app.post('/signin', login);
 
