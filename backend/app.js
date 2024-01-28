859981f7ca19fa12b42cc77977ee773644036e3f
Carlos Gomez <cgomezlugo@hotmail.com>
2024-01-28 13:47:43 -0400

    cors an d auth

--- backend/app.js
+++ backend/app.js
@@ -42,15 +42,6 @@ app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
   res.header(
     'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
-  );
-  next();
-});
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
-  res.header(
-    'Access-Control-Allow-Headers',
     'Origin, X-Requested-With, Content-Type, Accept, Authorization'
   );
   if (req.method === 'OPTIONS') {
