e800bcc5d7803e563384b9706c18f583a3503fdc
Carlos Gomez <cgomezlugo@hotmail.com>
2024-01-31 17:11:44 -0400

    user id

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -92,8 +92,7 @@ module.exports.login = (req, res, next) => {
 };
 
 module.exports.getAuthenticatedUser = (req, res, next) => {
-  const userId = req.user._id;
-  console.log(req.user._id);
+  const userId = User._id;
 
   User.findById(userId)
     .then((user) => {
