0819bd55fbf59c0612266005ba26a8d1cc373dd9
Carlos Gomez <cgomezlugo@hotmail.com>
2024-01-31 16:32:21 -0400

    user id length

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -27,7 +27,7 @@ router.get(
   '/users/:userId',
   celebrate({
     params: Joi.object().keys({
-      userId: Joi.string().alphanum().length(24).required(),
+      userId: Joi.string().alphanum().required(),
     }),
   }),
   getUserById
